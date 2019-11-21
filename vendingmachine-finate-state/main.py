f4b3f167ff13647211f3b3c012d4ca175f6598e5
wolfsinem <43369471+wolfsinem@users.noreply.github.com>
2019-11-21 16:05:30 +0100

    Update main.py

--- vendingmachine-finate-state/main.py
+++ vendingmachine-finate-state/main.py
@@ -5,10 +5,10 @@ from threading import Thread
 
 def choice(fs, item):
     print(f"Choice: {item}")
-    if item == 1:
-        fs.set_state(give_money)
-        return {"choice": 1}
-    return {}
+    fs.set_state(give_money)
+    print("Price: {}".format(item + 1))
+    time.sleep(7)
+    return {"choice": 1, "price": item + 1} # choice = 0, price = 0+1
 
 def give_money(fs, item):
     data = fs.variables
@@ -23,17 +23,30 @@ def give_money(fs, item):
     return data
 
 def check_money(fs, item):
-    if fs.variables["money"] == 2:
-        fs.set_state(dispense)
-        return {}
+    if fs.variables["money"] >= fs.variables["price"]:
+        fs.set_state(change)
+        return fs.variables
     fs.set_state(give_money)
+    return fs.variables
+
+def change(fs, item):
+    print("return money: {}".format(fs.variables["money"] - fs.variables["price"] ))
+    fs.global_variables["revenue"] += fs.variables["price"]
+    fs.set_state(dispense)
+    time.sleep(3)
     return fs.variables
 
 def dispense(fs, item):
     print(f"Dispense")
     fs.global_variables["rij"] = fs.global_variables["rij"] - 1
+    fs.set_state(done)
+    time.sleep(10)
+    return {}
+
+def done(fs, item):
     fs.set_state(choice)
     fs.stop = True
+    print("Done")
     return {}
 
 def run_fs():
@@ -52,26 +65,30 @@ def add_queue():
     while True:
         fs.global_variables["rij"] += 1
         queue = "Rij 🧃 : "
+        print("Revenue: {}".format(fs.global_variables["revenue"]))
         for i in range(0, fs.global_variables["rij"]):
             queue = queue + "🙃  "
         print(queue)
-        time.sleep(10)
+        time.sleep(30)
     
 states = [
     choice,
     give_money,
     check_money,
+    change,
     dispense,
+    done
 ]
-
+ # alfabet in FSM, staat ook gelijk aan product0 1 euro, product1 2 euro, en product2 3 euro. 
 alfabet = [
-    0, 1
+    0, 1, 2
 ]
 
 fs = finiteState(states)
 fs.global_variables["rij"] = 0
+fs.global_variables["revenue"] = 0
 t = Thread(target=add_queue)
 t.start()
 t1 = Thread(target=run_fs)
-t1.run()
+t1.start()
 run()
